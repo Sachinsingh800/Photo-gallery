1ebd03f60e8f7985cbcb6cdae0020616ebf4829f
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-05-04 16:34:07 +0530

    add uploading functionality to photo page

--- src/Components/Pages/Photo/Photo.js
+++ src/Components/Pages/Photo/Photo.js
@@ -1,6 +1,7 @@
 import React ,{useState} from 'react'
 import NavigationBar from '../../NavigationBar/NavigationBar';
 import style from "./Photo.module.css"
+import Image from "../../../asset/upload.png"
 
 function Photo() {
     const [image, setImage] = useState(null);
@@ -11,10 +12,24 @@ function Photo() {
     };
   return (
     <div className={style.main}>
-        <NavigationBar />
-      <h1>Photo</h1>
-      <input type="file" onChange={onImageChange} className="filetype" />
-      {image ? <img src={image} alt="preview image" /> : ''}
+        <div className={style.nav}><NavigationBar /></div>
+        <div className={style.container}>
+      <div className={style.imgContainer}>
+      {image ? <img className={style.image} src={image} alt="preview image" /> :
+       <div> 
+         <label htmlFor='image' ><img className={style.icon} src={Image}  atl="icon" /></label>
+      <input id='image' hidden type="file" onChange={onImageChange} className="filetype" />
+      <h1>upload image here</h1>
+      </div>}
+      </div>
+      <form className={style.form}>
+      <label htmlFor="Name">Name </label>
+        <input id='Name' placeholder='write name of your photo'  />
+        <label htmlFor="Discription">Discription</label>
+        <textarea id='Discription' placeholder='write something about your photo'  />
+      </form>
+        </div>
+     
       
     </div>
   )
