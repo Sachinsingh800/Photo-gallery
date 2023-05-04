0b94f6aa3116b8b117042b9a5c5f1708c6a1f674
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-05-04 19:10:20 +0530

    photo page

--- src/Components/Pages/Photo/Photo.js
+++ src/Components/Pages/Photo/Photo.js
@@ -1,39 +1,58 @@
-import React ,{useState} from 'react'
-import NavigationBar from '../../NavigationBar/NavigationBar';
-import style from "./Photo.module.css"
-import Image from "../../../asset/upload.png"
+import React, { useState } from "react";
+import NavigationBar from "../../NavigationBar/NavigationBar";
+import style from "./Photo.module.css";
+import Image from "../../../asset/upload.png";
 
 function Photo() {
-    const [image, setImage] = useState(null);
-    const onImageChange = (event) => {
-      if (event.target.files[0]) {
-        setImage(URL.createObjectURL(event.target.files[0]));
-      }
-    };
+  const [image, setImage] = useState(null);
+  const onImageChange = (event) => {
+    if (event.target.files[0]) {
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+  };
   return (
     <div className={style.main}>
-        <div className={style.nav}><NavigationBar /></div>
-        <div className={style.container}>
-      <div className={style.imgContainer}>
-      {image ? <img className={style.image} src={image} alt="preview image" /> :
-       <div> 
-         <label htmlFor='image' ><img className={style.icon} src={Image}  atl="icon" /></label>
-      <input id='image' hidden type="file" onChange={onImageChange} className="filetype" />
-      <h1>upload image here</h1>
-      </div>}
+      <div className={style.nav}>
+        <NavigationBar />
       </div>
-      <form className={style.form}>
-      <label htmlFor="Name">Name </label>
-        <input id='Name' placeholder='write name of your photo'  />
-        <label htmlFor="Discription">Discription</label>
-        <textarea id='Discription' placeholder='write something about your photo'  />
-        <button className={style.submit}>Submit</button>
-      </form>
+      <div className={style.container}>
+        <div className={style.imgContainer}>
+          {image ? (
+            <img className={style.image} src={image} alt="preview image" />
+          ) : (
+            <div className={style.uploadblock}>
+              <label htmlFor="image">
+                <img className={style.icon} src={Image} atl="icon" />
+              </label>
+              <input
+                id="image"
+                hidden
+                type="file"
+                onChange={onImageChange}
+                className="filetype"
+              />
+              <label htmlFor="image">
+                <h1>upload image here</h1>
+              </label>
+            </div>
+          )}
         </div>
-     
-      
+        <form className={style.form}>
+          <label htmlFor="Name">Name </label>
+          <input id="Name" placeholder="write name of your photo" />
+          <label htmlFor="Discription">Discription</label>
+          <textarea
+            id="Discription"
+            rows="3"
+            cols="55"
+            maxlength="150"
+            placeholder="write something about your photo"
+          />
+          <button className={style.submit}>Submit</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Photo
+export default Photo;
